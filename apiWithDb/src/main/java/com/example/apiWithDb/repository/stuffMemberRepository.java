6296a4a7cd99623af0d74b75b1f52019407eede0
NikolaiChelovekich <nikolay.gonchar.2002@mail.ru>
2024-04-26 19:29:17 +0300

    refactor/employee

--- apiWithDb/src/main/java/com/example/apiWithDb/repository/stuffMemberRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.apiWithDb.repository;
-
-import com.example.apiWithDb.model.StuffMember;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface stuffMemberRepository extends JpaRepository<StuffMember, String> {
-
-}
