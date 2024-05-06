9fcdceb526b873cae9509f4e1977aefc9319a723
NikolaiChelovekich <nikolay.gonchar.2002@mail.ru>
2024-05-06 16:12:22 +0300

    refactor/employee_entity

--- apiWithDb/src/main/java/com/example/apiWithDb/entities/Employee.java
+++ apiWithDb/src/main/java/com/example/apiWithDb/entities/Employee.java
@@ -3,59 +3,20 @@ package com.example.apiWithDb.entities;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name="employeeInfo")
-public class Employee
-{
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Employee {
+
     @Id
     private String employeeId;
     private String employeeName;
     private String employeeSecName;
     private String employeePhoto;
-
-    public Employee() {
-    }
-
-    public Employee(String employeePhoto,
-                    String employeeSecName,
-                    String employeeName,
-                    String employeeId) {
-            this.employeePhoto = employeePhoto;
-            this.employeeSecName = employeeSecName;
-            this.employeeName = employeeName;
-            this.employeeId = employeeId;
-    }
-
-    public String getEmployeeId() {
-        return employeeId;
-    }
-
-    public void setEmployeeId(String employeeId) {
-        this.employeeId = employeeId;
-    }
-
-    public String getEmployeePhoto() {
-        return employeePhoto;
-    }
-
-    public void setEmployeePhoto(String employeePhoto) {
-        this.employeePhoto = employeePhoto;
-    }
-
-    public String getEmployeeSecName() {
-        return employeeSecName;
-    }
-
-    public void setEmployeeSecName(String employeeSecName) {
-        this.employeeSecName = employeeSecName;
-    }
-
-    public String getEmployeeName() {
-        return employeeName;
-    }
-
-    public void setEmployeeName(String employeeName) {
-        this.employeeName = employeeName;
-    }
 }
